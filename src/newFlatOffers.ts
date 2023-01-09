4df35abe5150f1a69b43fdde0682cd85623924c8
maciekkwiet <maciek.kwiet36@gmail.com>
2023-01-10 00:10:42 +0100

    add otodom to bot

--- src/newFlatOffers.ts
+++ src/newFlatOffers.ts
@@ -7,7 +7,7 @@ export const getNewFlatOffers = async (TelegramBot: TelegramBot, logger: winston
     logger.info('Sending first message...')
     TelegramBot.sendMessage(-1001870792878, 'Bot uruchomiony poprawnie')
     setInterval(async () => {
-      const response = await fetch("https://www.olx.pl/api/v1/offers/?offset=0&limit=50&category_id=15&region_id=3&city_id=19701&sort_by=created_at%3Adesc&filter_enum_rooms%5B0%5D=two&filter_enum_rooms%5B1%5D=three&filter_float_m%3Afrom=40&filter_float_price%3Afrom=2500&filter_float_price%3Ato=3000&filter_refiners=spell_checker&facets=%5B%7B%22field%22%3A%22district%22%2C%22fetchLabel%22%3Atrue%2C%22fetchUrl%22%3Atrue%2C%22limit%22%3A30%7D%2C%7B%22field%22%3A%22category_without_exclusions%22%2C%22fetchLabel%22%3Atrue%2C%22fetchUrl%22%3Atrue%2C%22limit%22%3A10%7D%5D&sl=183f1984023x39a26426", {
+      const responseOLX = await fetch("https://www.olx.pl/api/v1/offers/?offset=0&limit=50&category_id=15&region_id=3&city_id=19701&sort_by=created_at%3Adesc&filter_enum_rooms%5B0%5D=two&filter_enum_rooms%5B1%5D=three&filter_float_m%3Afrom=40&filter_float_price%3Afrom=2500&filter_float_price%3Ato=3000&filter_refiners=spell_checker&facets=%5B%7B%22field%22%3A%22district%22%2C%22fetchLabel%22%3Atrue%2C%22fetchUrl%22%3Atrue%2C%22limit%22%3A30%7D%2C%7B%22field%22%3A%22category_without_exclusions%22%2C%22fetchLabel%22%3Atrue%2C%22fetchUrl%22%3Atrue%2C%22limit%22%3A10%7D%5D&sl=183f1984023x39a26426", {
         "headers": {
           "accept": "*/*",
           "accept-language": "pl",
@@ -30,10 +30,34 @@ export const getNewFlatOffers = async (TelegramBot: TelegramBot, logger: winston
         "body": undefined,
         "method": "GET"
       });
+
+      const responseOtodom = await fetch("https://www.otodom.pl/_next/data/BQYrPyhzIUlk1YK9rR-JO/pl/oferty/wynajem/mieszkanie/wroclaw.json?distanceRadius=0&page=1&limit=36&market=ALL&ownerTypeSingleSelect=ALL&priceMin=2500&priceMax=3000&areaMin=40&locations=%5Bcities_6-39%5D&by=DEFAULT&direction=DESC&viewType=listing&lang=pl&searchingCriteria=wynajem&searchingCriteria=mieszkanie&searchingCriteria=wroclaw", {
+        "headers": {
+          "accept": "*/*",
+          "accept-language": "pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7",
+          "baggage": "sentry-environment=otodompl-prd,sentry-release=re-frontend-platform%402023-01-09_14.01-otodompl,sentry-transaction=%2F%5Blang%5D%2Foferty%2F%5B%5B...searchingCriteria%5D%5D,sentry-public_key=a87771f160f04e019f364675c25c0588,sentry-trace_id=a22a77d493bd4a34a2718f3aacbee100,sentry-sample_rate=0.0001",
+          "cache-control": "no-cache",
+          "pragma": "no-cache",
+          "sec-ch-ua": "\"Not?A_Brand\";v=\"8\", \"Chromium\";v=\"108\", \"Google Chrome\";v=\"108\"",
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": "\"macOS\"",
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-origin",
+          "sentry-trace": "a22a77d493bd4a34a2718f3aacbee100-a0d2c04ed218d698-0",
+          "cookie": "laquesisff=gre-12226#rer-165#rer-166#rst-73#rst-74; OptanonAlertBoxClosed=2022-10-19T18:42:38.638Z; OTAdditionalConsentString=1~89.2008.2072.2322.2465.2501.2999.3028.3225.3226.3231.3232.3234.3235.3236.3237.3238.3240.3244.3245.3250.3251.3253.3257.3260.3268.3270.3272.3281.3288.3290.3292.3293.3295.3296.3300.3306.3307.3308.3314.3315.3316.3318.3324.3327.3328.3330; _gcl_au=1.1.1122860254.1666204959; st_userID=GA1.2.1507911430.1666204959__unlogged; __gads=ID=4ffd260220d3f372:T=1666204958:S=ALNI_MZVANYH2eH0z-Z2o4VyrWgWESUP7g; laquesissu=294@survey_answer|1#314@ad_page|1#314@listing|1; _ga_MNTL052JB4=GS1.1.1671139974.9.0.1671139974.60.0.0; lang=pl; eupubconsent-v2=CPhF1LAPhF1LAAcABBENCyCsAP_AAH_AAAYgJFNf_X__b2_r-_5_f_t0eY1P9_7__-0zjhfdl-8N3f_X_L8X52M7vF36tq4KuR4ku3LBIUdlHOHcTUmw6okVryPsbk2cr7NKJ7PEmnMbOydYGH9_n1_z-ZKY7___f_7z_v-v___3____7-3f3__5___-__e_V__9zfn9_____9vP___9v-_9__________3_7997BIgAkw1biALsyxwZtowigRAjCsJDqBQAUUAwtEBhA6uCnZXAT6wgQAIBQBOBECHAFGDAIAABIAkIiAkCPBAIACIBAACABUIhAAxsAgsALAwCAAUA0LFGKAIQJCDIgIilMCAqRIKCeyoQSg_0NMIQ6ywAoNH_FQgI1kDFYEQkLByHBEgJeLJA8xRvkAIwQoBRKhWogAAA.f_gAD_gAAAAA; observed5_id_clipboard=63bc1c8d39b0f; observed5_sec_clipboard=lz1I2QndlnJH3c%2BOM9lp3Jf6lAS6a5Z5; _gid=GA1.2.1219320400.1673207269; ln_or=eyIxODYxOTA4IjoiZCJ9; __gfp_64b=a9UnyjjWEvuZprnegmOaC_VzmXpdMgkwdCG9PZ8tXjf.S7|1666204959; laquesis=remt-288@a#remt-395@a#remt-448@a#remt-457@b#remt-458@b#remt-507@c#see-1171@c#see-1189@a#see-1294@c#smr-1387@b#smr-1388@c#smr-1481@b#smr-1486@b#smr-954@b; PHPSESSID=4nov7815vjsdsvmi8egqgfn9i2; mobile_default=desktop; ninja_user_status=unlogged; optimizelyEndUserId=oeu1673282248579r0.02918823203480203; lastCatType=2; __gpi=UID=00000b758bdb18e9:T=1666204958:RT=1673299821:S=ALNI_MbPq1EmHjTrUzTyVHsWByhxBhughA; OptanonConsent=isGpcEnabled=0&datestamp=Mon+Jan+09+2023+22%3A31%3A48+GMT%2B0100+(czas+%C5%9Brodkowoeuropejski+standardowy)&version=6.32.0&isIABGlobal=false&hosts=&genVendors=V9%3A0%2C&consentId=b8d02efa-bf8a-432a-be63-a0ece378b77a&interactionCount=1&landingPath=NotLandingPage&groups=C0001%3A1%2CC0002%3A1%2CC0003%3A1%2CC0004%3A1%2Cgad%3A1%2CSTACK42%3A1&geolocation=PL%3B02&AwaitingReconsent=false; _uetsid=1f209b30902511ed9757d15a99df2773; _uetvid=49d60080f57311ec998793fab994f6ba; _ga=GA1.2.1025374861.1666205631; _ga_MJFMKPTTMP=GS1.1.1673299789.5.1.1673299921.30.0.0; lqstatus=1673302560|18598708711x755b042e|see-1189#see-1294#smr-1481#smr-1486#remt-448#remt-457#remt-507|1670445309|314; _gat_UA-124076552-11=1; _gat_clientNinja=1; onap=183f18df57cx8ec294a-12-18598708711x755b042e-179-1673303179; _ga_HL51DW3Q97=GS1.1.1673299789.13.1.1673301379.54.0.0",
+          "Referer": "https://www.otodom.pl/pl/oferty/wynajem/mieszkanie/wroclaw?distanceRadius=0&page=1&limit=36&market=ALL&ownerTypeSingleSelect=ALL&priceMin=2500&priceMax=3000&areaMin=40&locations=%5Bcities_6-39%5D&by=DEFAULT&direction=DESC&viewType=listing",
+          "Referrer-Policy": "no-referrer-when-downgrade"
+        },
+        "body": undefined,
+        "method": "GET"
+      });
   
-      const result = await response.json()
+      const resultOLX = await responseOLX.json()
+      const resultOtodom = await responseOtodom.json()
   
-      const urls = result.data.map((option: any) => option.url.toString()) as string[]
+      const urlsOLX = resultOLX.data.map((option: any) => option.url.toString()) as string[]
+      const urlsOtodom = resultOtodom.pageProps.data.searchAds.items.map((option: any) => `https://www.otodom.pl/pl/oferta/${option.slug.toString()}`) as string[]
 
       const newFlatOffers = await fs.readFileSync('newFlatOffers.json', 'utf8')
       const oldFlatOffers = await fs.readFileSync('oldFlatOffers.json', 'utf8')
@@ -43,9 +67,9 @@ export const getNewFlatOffers = async (TelegramBot: TelegramBot, logger: winston
       const newOldFlatOffers = [...new Set([...parsedNewFlatOffers, ...parsedOldFlatOffers])] as string[]
 
       fs.writeFile('oldFlatOffers.json', JSON.stringify(newOldFlatOffers), () => {})
-      fs.writeFile('newFlatOffers.json', JSON.stringify(urls), () => {})
+      fs.writeFile('newFlatOffers.json', JSON.stringify([...urlsOLX, ...urlsOtodom]), () => {})
 
-      const newOffers = urls.filter((url: string) => !newOldFlatOffers.includes(url)) ?? []
+      const newOffers = [...urlsOLX, ...urlsOtodom].filter((url: string) => !newOldFlatOffers.includes(url)) ?? []
 
       if (newOffers.length > 0) {
         logger.info(`Znaleziono nowe mieszkanie: ${newOffers.toString()}`)
